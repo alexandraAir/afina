fa17f58213bd75d4fe0c91531d672e517a91aa5c
alexandraAir <korneevasandra@gmail.com>
2019-11-06 22:51:51 +0300

    hw5 mt_nonblocking

--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -4,6 +4,10 @@
 #include <cstring>
 
 #include <sys/epoll.h>
+#include <spdlog/logger.h>
+#include <afina/execute/Command.h>
+#include <protocol/Parser.h>
+
 
 namespace Afina {
 namespace Network {
@@ -11,12 +15,13 @@ namespace MTnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps, std::shared_ptr<spdlog::logger> pl) :
+        _socket(s), _logger(std::move(pl)), pStorage(std::move(ps)) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
     }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return _is_alive; }
 
     void Start();
 
@@ -32,6 +37,23 @@ private:
 
     int _socket;
     struct epoll_event _event;
+
+    std::mutex _mutex;
+
+    std::shared_ptr<spdlog::logger> _logger;
+
+    bool _is_alive = true;
+    char client_buffer[4096];
+    std::size_t arg_remains;
+    Protocol::Parser parser;
+    std::string argument_for_command;
+    std::unique_ptr<Execute::Command> command_to_execute;
+    std::shared_ptr<Afina::Storage> pStorage;
+
+    std::vector<std::string> buffer;
+
+    int cur_pos = 0;
+    int now_pos = 0;
 };
 
 } // namespace MTnonblock
